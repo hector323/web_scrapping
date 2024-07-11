5565ecb3c4dac95c0cc2a6805426b642f424bf89
Hector Salgado <hector323@gmail.com>
2024-07-10 17:51:12 -0700

    Add changes

--- books2scrape.py
+++ books2scrape.py
@@ -4,15 +4,16 @@ import csv
 import re
 import pandas as pd
 
+
 # Example usage
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
 def scrape_book_results_page(page_num, headers):
     """
-    scrape_book_results_page 
-    
-    :param page_num: page number 
+    scrape_book_results_page
+
+    :param page_num: page number
     :param headers: request headers
     :return: dictionary with the following keys:
         page_url: The URL of the books results page
@@ -20,11 +21,11 @@ def scrape_book_results_page(page_num, headers):
         soup: The BeautifulSoup object created from the source code
         book_urls: A list of the URLs for each book on this page
     """
-    
+
     # For each results page, scrape the URLs for each book page
-    page_url = f'https://books.toscrape.com/catalogue/page-{page_num}.html' 
+    page_url = f'https://books.toscrape.com/catalogue/page-{page_num}.html'
     response = requests.get(page_url, headers=headers)
-    
+
     # Check if the response is successful
     if response.status_code != 200:
         return {
@@ -33,14 +34,14 @@ def scrape_book_results_page(page_num, headers):
             'soup': None,
             'book_urls': []
         }
-    
+
     text = BeautifulSoup(response.text, 'html.parser')
-    
+
     # Get the book page URLs and add the domain, then add to book page URL list
     book_divs = text.find_all('div', attrs={'class': 'image_container'})
     book_urls = [tag.find('a').get('href') for tag in book_divs]
     complete_book_urls = [f'https://books.toscrape.com/catalogue/{end_part}' for end_part in book_urls]
-    
+
     # return book_dict
     return {
         'page_url': page_url,
@@ -51,28 +52,28 @@ def scrape_book_results_page(page_num, headers):
 
 
 
-        
+
 def scrape_book_product_page(book_url, headers=headers):
     """
-    scrape_book_product_page docstring (book_product_url, headers): 
-    
-    This function takes a book product URL (the URL for the book product page) 
-    
-    and a headers dictionary as arguments 
-    
+    scrape_book_product_page docstring (book_product_url, headers):
+
+    This function takes a book product URL (the URL for the book product page)
+
+    and a headers dictionary as arguments
+
     and returns a dictionary with the following keys:
         book_url: The URL of the book product page
         response: The Response object of that page
         soup: The BeautifulSoup object created from the source code
     """
-    
+
 
     response = requests.get(book_url, headers=headers)
     text = BeautifulSoup(response.text, 'html.parser')
-    
+
     if response.status_code != 200:
         raise Exception(f'The status code is not 200! It is {response.status_code}.')
-    
+
     # return book_dict
     return {
         'book_url': book_url,
@@ -84,16 +85,16 @@ def scrape_book_product_page(book_url, headers=headers):
 def scrape_book_range(page_range, filename, headers=headers):
     """
     scrape_book_range docstring
-    
-    
-    scrape_book_range(page_range, filename, headers): 
-    This function takes a page range (range object), a filename for the a CSV file, 
-    and a headers dictionary as arguments and will use the other two functions to scrape the book information 
-    for every book found in the specified page range. 
-    This book information should be saved as separate rows in a CSV file 
+
+
+    scrape_book_range(page_range, filename, headers):
+    This function takes a page range (range object), a filename for the a CSV file,
+    and a headers dictionary as arguments and will use the other two functions to scrape the book information
+    for every book found in the specified page range.
+    This book information should be saved as separate rows in a CSV file
     (see if you can include the CSV file writing code in this function).
-    
-    
+
+
     :param page_range: range of int
     :param filename: filename
     :param headers: request headers
@@ -101,7 +102,7 @@ def scrape_book_range(page_range, filename, headers=headers):
     """
     acceptable_page_range = list(range(1,51))
     page_range = [i for i in page_range if i in acceptable_page_range]
-    
+
     # Create a new CSV file
     with open(filename, 'w', encoding = 'utf-8', newline='') as csvfile:
         # Create the CSV writer object
@@ -109,14 +110,14 @@ def scrape_book_range(page_range, filename, headers=headers):
         # Write the initial header row
         book_writer.writerow(['title', 'price_in_pounds', 'avg_rating', 'genre', 'upc', 'num_books_available'])
         stars_map = {'One': 1 , 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5}
-        
+
         for page in page_range:
               results = scrape_book_results_page(page, headers)
               for book_url in results['book_urls']:
                   book = scrape_book_product_page(book_url)
                   text = book['soup']
-                  
-                
+
+
                   # Get title
                   title = text.find('div', attrs = {'class': 'col-sm-6 product_main'}).find('h1').string
                   # Get price
@@ -132,18 +133,18 @@ def scrape_book_range(page_range, filename, headers=headers):
                   upc = tr_tag.find('td').string
                   # Get number of books available
                   num_books_available = text.find('p', attrs = {'class':'instock availability'}).get_text()
-              
+
                   # Store info in the dictionary
                   book_dict = {}
                   book_dict['title'] = title
-                
+
                   book_dict['price_in_pounds'] = float(re.search('[0-9.]+' , price_in_pounds).group())
-                    
+
                   book_dict['avg_rating'] = stars_map.get(avg_rating)
                   book_dict['genre'] = genre
                   book_dict['upc'] = upc
                   #book_dict['num_books_available'] = num_books_available
-                  book_dict['num_books_available'] = ''.join(re.findall(r'\b\d+\b', num_books_available))  
+                  book_dict['num_books_available'] = ''.join(re.findall(r'\b\d+\b', num_books_available))
                   #book_dict['num_books_available'] = int(re.search('\d+', num_books_available).group())
                   book_writer.writerow(book_dict.values())
 
